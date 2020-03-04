f44b4fd56a28b0094083d2f1b9e21d224bc028e1
Code_Cube <therealcodecube@gmail.com>
2020-03-04 15:50:08 -0800

    HandAvatar does proper animations upon hovering over a card.

--- Assets/Scripts/CardStuff/HandAvatar.cs
+++ Assets/Scripts/CardStuff/HandAvatar.cs
@@ -7,9 +7,12 @@ public class HandAvatar : MonoBehaviour
     public CardAvatar cardAvatarPrefab;
     public Card[] cards;
     public float CARD_WIDTH, CARD_HEIGHT;
-    public float CARD_SPACING = 2.0f;
-    public float CARD_ROTATION = 5.0f;
+    public float CARD_SPACING = 400.0f;
+    public float CARD_ROTATION = 3.0f;
+    public float EMPHASIZED_SCALE = 1.8f;
+
     private List<CardAvatar> avatars = new List<CardAvatar>();
+    private CardAvatar emphasized, waitingToBeEmphasized;
 
     void Start()
     {
@@ -26,29 +29,71 @@ public class HandAvatar : MonoBehaviour
 
     private void Emphasize(CardAvatar avatar)
     {
-        Debug.Log("Emphasize");
+        if (emphasized == null)
+        {
+            emphasized = avatar;
+            UpdateAvatarTransforms();
+        }
+        else
+        {
+            waitingToBeEmphasized = avatar;
+        }
     }
 
     private void Deemphasize(CardAvatar avatar)
     {
-        Debug.Log("Deemphasize");
+        if (avatar == emphasized)
+        {
+            emphasized = waitingToBeEmphasized;
+            UpdateAvatarTransforms();
+        }
+        else if (avatar == waitingToBeEmphasized)
+        {
+            waitingToBeEmphasized = null;
+        }
     }
 
     private void UpdateAvatarTransforms()
     {
+        int emphasizedIndex = avatars.IndexOf(emphasized);
+        // Handles both cases of `emphasized` being null and of being something not in the list.
+        bool somethingIsEmphasized = emphasizedIndex > -1;
         for (int index = 0; index < avatars.Count; index++)
         {
             float transformAmount = ((float)index) - ((float)avatars.Count - 1) / 2.0f;
+            if (somethingIsEmphasized)
+            {
+                if (index < emphasizedIndex)
+                {
+                    transformAmount -= 0.2f;
+                }
+                else if (index > emphasizedIndex)
+                {
+                    transformAmount += 0.2f;
+                }
+            }
             float angle = transformAmount * CARD_ROTATION;
             Vector3 position = new Vector3(
                 Mathf.Sin(angle * Mathf.Deg2Rad),
                 Mathf.Cos(angle * Mathf.Deg2Rad) - 1,
-                0
+                // The emphasized card should render on top of everything else.
+                index == emphasizedIndex ? 0 : 1
             ) * CARD_SPACING;
 
             CardAvatar avatar = avatars[index];
             avatar.transform.localPosition = position;
             avatar.transform.localRotation = Quaternion.AngleAxis(angle, Vector3.back);
+
+            Vector3 scale;
+            if (index == emphasizedIndex)
+            {
+                scale = new Vector3(EMPHASIZED_SCALE, EMPHASIZED_SCALE, EMPHASIZED_SCALE);
+            }
+            else
+            {
+                scale = new Vector3(1, 1, 1);
+            }
+            avatar.transform.localScale = scale;
         }
     }
 
