fb6c1b041c3b3c4f0a889b817867b3f3c37532c1
Kyle Townsend <townsend.kyle.kt@gmail.com>
2020-03-21 17:19:27 -0700

    Set up some files to work on

---
+++ Assets/Scripts/BuyItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyItem : MonoBehaviour
+{
+    //Likely to be more than one type of item a player can buy.
+    public GameObject item;
+    public int cost;
+
+    public void Buy()
+    {
+        //Checks type of item and takes appropriate action.
+        /*if (item.GetComponent<CardAvatar>() != null)
+        {
+            //Deck.instance.Add(item.displaying);
+        }*/
+
+        Currency.SubtractCurrency(cost);
+        Debug.Log(Currency.GetCurrency());
+    }
+}
