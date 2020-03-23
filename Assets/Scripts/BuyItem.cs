aa907d625e4f0863e19fea87a540f8ac09eaa45d
Kyle Townsend <townsend.kyle.kt@gmail.com>
2020-03-23 01:46:01 -0700

    Basic implementation of buying items.

--- Assets/Scripts/BuyItem.cs
+++ Assets/Scripts/BuyItem.cs
@@ -3,32 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//Script is attached to a buyable item.
+
 public class BuyItem : MonoBehaviour
 {
-    //Possible solution: In script that adds and displays buyable cards (and other items), add
-    //BuyItem script to each and take appropriate action. May need to have UI functions from 
-    //this script (ie MouseOver, click to buy, etc.)
+    //Cost for item.
+    private int cost;
 
-    //Likely to be more than one type of item a player can buy.
-    public GameObject item;
-    public int cost;   
-    public Card cardInfo;   //Likely a placeholder variable. Currently necessary when testing.
-
-    private void Start()
+    private void OnMouseDown()
     {
-        //Likely not going to be needed in final version. Currently necessary when testing.
-        item.GetComponent<CardAvatar>().displaying = cardInfo;
+        Buy();
     }
 
-    public void Buy()
+    private void Buy()
     {
         //Checks if player can afford item.
         if (Currency.GetCurrency() >= cost)
         {
             //Checks type of item and takes appropriate action.
-            if (item.GetComponent<CardAvatar>() != null)
+            //Currently, only item type is card, so checks for CardAvatar.
+            if (gameObject.GetComponent<CardAvatar>() != null)
             {
-                Deck.instance.Add(item.GetComponent<CardAvatar>().displaying);
+                Deck.instance.Add(gameObject.GetComponent<CardAvatar>().displaying);
             }
 
             Currency.SubtractCurrency(cost);
@@ -40,9 +36,8 @@ public class BuyItem : MonoBehaviour
         }
     }
 
-    //For debug purposes. Puts player in battle where the new bought card will eventually be seen.
-    public void Change()
+    public void SetCost(int cost)
     {
-        SceneManager.LoadScene("SampleScene");
+        this.cost = cost;
     }
 }
---
+++ Assets/Scripts/MerchantUIModified.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MerchantUIModified : MonoBehaviour
+{
+    public float CARD_SPACING;
+
+    [SerializeField]
+    private List<CardAvatar> cardsForSell;
+    public Card cardInfoPlaceholder;    //Used for testing purposes.
+
+    private void Start()
+    {
+        DisplayUI();
+    }
+
+    public void AddToMerchantShop(CardAvatar card)
+    {
+        cardsForSell.Add(card);
+    }
+
+    public void DisplayUI()
+    {
+        for (int i = 0; i < cardsForSell.Count; i++)
+        {
+            float transformAmount = ((float)i) - ((float)cardsForSell.Count - 1) / 2;
+            float angle = transformAmount * 3.0f;
+            Vector3 position = new Vector3(
+                Mathf.Sin(angle * Mathf.Deg2Rad),
+                0,
+                0
+                ) * CARD_SPACING;
+            cardsForSell[i].transform.localPosition = position;
+            //cardsForSell[i].transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+
+            cardsForSell[i].displaying = cardInfoPlaceholder;   //Temporary, for testing purposes.
+
+            cardsForSell[i].gameObject.AddComponent<BuyItem>();
+            SetBuyAttributes(cardsForSell[i]);
+        }
+    }
+
+    private void SetBuyAttributes(CardAvatar curCard)
+    {
+        //Idea: we could have a nested class in a style similar to Josh's DeckTester class.
+        //Then, we could set a cost for each item added to be bought, instead of needing to check
+        //the name. This class would be the type for the cardsForSell list instead of the
+        //current CardAvatar. (This would require some tweaking in the code to work properly.)
+        switch (curCard.displaying.cardName)
+        {
+            case "Damage10":
+                curCard.GetComponent<BuyItem>().SetCost(10);
+                break;
+            default:
+                Debug.Log("Card has no set cost.");
+                break;
+        }
+    }
+}
