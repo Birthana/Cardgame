f372ba81a64db703339e83244b9f4cac28103dc6
Thana Sithisombath <sithisombath@cpp.edu>
2020-04-03 15:24:20 -0700

    test card effect/card.

---
+++ Assets/Scripts/CardEffectTemplates/DealDamageScaledByPortal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Card Game Stuff/Effects/Deal Damage Scaled By Portal", fileName = "DealDamageScaledByPortal")]
+public class DealDamageScaledByPortal : CardEffect
+{
+    public int amount;
+
+    public override void ApplyEffect(ActionContext context)
+    {
+        context.damageBoost = 2;
+        foreach (FieldEntity target in context.targets)
+        {
+            target.TakeDamage(context.ComputeDamage(amount));
+        }
+    }
+}
