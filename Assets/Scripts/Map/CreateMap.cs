7881f59527fc1ede3de52e221b3f2f19e9105a50
Kyle Townsend <townsend.kyle.kt@gmail.com>
2020-04-05 00:48:19 -0700

    Updated createmap script

--- Assets/Scripts/Map/CreateMap.cs
+++ Assets/Scripts/Map/CreateMap.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class CreateMap : MonoBehaviour
 {
+    //WARNING: Script does not yet have safety checks
+    //If EVENT_SPACING is too large, or numToGenerate is too big, script may result in an infinite loop.
+    //Maybe implement a better way to generate positions?
+    //For example, make sure position is within acceptable range at generation time,
+    //so no need for checking for space.
+    //Also might want to make elipse from top and bottom similar to gif in discord.
+    //To do this, could generate y first, and then figure out acceptable bounds for x.
+    //
+    //Idea for connecting events:
+    //Connect paths from left to right. Progress by each Y coord of events.
+    //Prevent overlapping, if overlap or no empty events to connect, connect to same event.
+
     private float SCREEN_HORIZONTAL_SPACING = 60f;  //Minimum spacing between sides of screen and events.
     private float SCREEN_VERTICAL_SPACING = 15f;  //Minimum spacing between top and bottom of screen and events.
-    private float EVENT_SPACING = 5f;  //Minimum spacing between each event.
+    private float EVENT_SPACING = 15f;  //Minimum spacing between each event.
 
     private Vector3 bottomPosition;
     private Vector3 topPosition;
     private List<Vector3> otherPositions = new List<Vector3>();
-    private int numToGenerate = 10;
+    private int numY = 3;   //Number of Y coords to generate and spawn an event at.
+    private int minPerY = 1;    //Minimum number of events to generate for every Y coord used.
+    private int maxPerY = 2;    //Maximum number of events to generate for every Y coord used.
 
     public GameObject enemyEncounter;
     public GameObject town;
@@ -40,21 +54,64 @@ public class CreateMap : MonoBehaviour
     private void Fill()
     {
         Vector3 newPosition;
+        float newY;
+        float newX;
+        float verticalSpacing = (topPosition.y - bottomPosition.y) / (numY + 1);
+
+        //Used to calculate horizontal spacing between events.
+        Vector3 leftMostPosition = Camera.main.ScreenToWorldPoint(new Vector3(
+            SCREEN_HORIZONTAL_SPACING, 0));
+        Vector3 rightMostPosition = Camera.main.ScreenToWorldPoint(new Vector3(
+            Screen.width - SCREEN_HORIZONTAL_SPACING, 0));
+        float horizontalSpacing;
+
+        int numX;
+
+        for (int i = 1; i <= numY; i++)
+        {
+            numX = Random.Range(minPerY, maxPerY + 1);
+            for (int j = 1; j <= numX; j++)
+            {
+                horizontalSpacing = Vector3.Distance(leftMostPosition, rightMostPosition) / (numX + 1);
+                newY = bottomPosition.y + (i * verticalSpacing);
+                /*newPosition = Camera.main.ScreenToWorldPoint(new Vector3(
+                    Random.Range(SCREEN_HORIZONTAL_SPACING, Screen.width - SCREEN_HORIZONTAL_SPACING),
+                    0));*/
+                newX = leftMostPosition.x + (j * horizontalSpacing);
+                newPosition = new Vector3(newX, newY, 0);
+                /*newPosition.y = newY;
+                newPosition.x = newX;
+                newPosition.z = 0;*/
+                Instantiate(enemyEncounter, newPosition, Quaternion.identity);
+            }
+        }
+
+        /*
+        Vector3 newPosition;
         Vector3 temp;
 
         for (int i = 0; i < numToGenerate; i++)
         {
+            int counter = 0;
             do
             {
                 temp = Camera.main.ScreenToWorldPoint(new Vector3(
                     Random.Range(SCREEN_HORIZONTAL_SPACING, Screen.width - SCREEN_HORIZONTAL_SPACING),
-                    Random.Range(SCREEN_VERTICAL_SPACING,Screen.height - SCREEN_VERTICAL_SPACING)));
+                    Random.Range(SCREEN_VERTICAL_SPACING + EVENT_SPACING, 
+                    Screen.height - SCREEN_VERTICAL_SPACING - EVENT_SPACING)));
                 newPosition = new Vector3(temp.x, temp.y, 0);
+
+                counter++;
+                if (counter > 10)
+                {
+                    Debug.LogError("Infinite loop?");
+                    break;
+                }
             } while (!EnoughSpacing(newPosition));
 
             otherPositions.Add(newPosition);
             Instantiate(enemyEncounter, newPosition, Quaternion.identity);
-        }
+        }*/
     }
 
     //Returns true if a position has enough spacing from other already used.
