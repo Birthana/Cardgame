f0ca45b734da7d3bc7de6343036b3fe0e5169a82
Thana Sithisombath <sithisombath@cpp.edu>
2020-04-04 22:28:50 -0700

    simple ui. testing async additive loading.

---
+++ Assets/Scripts/UITest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UITest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SceneManager.LoadSceneAsync("PlayerUI", LoadSceneMode.Additive);
+    }
+}
