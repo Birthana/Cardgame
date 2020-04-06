0442f620ee3811bf54e37b842dbdcb2887562036
Kyle Townsend <townsend.kyle.kt@gmail.com>
2020-04-06 13:29:59 -0700

    Updated map script.

--- Assets/Scripts/Map/CreateMap.cs
+++ Assets/Scripts/Map/CreateMap.cs
@@ -4,20 +4,9 @@ using UnityEngine;
 
 public class CreateMap : MonoBehaviour
 {
-    //WARNING: Script does not yet have safety checks
-    //If EVENT_SPACING is too large, or numToGenerate is too big, script may result in an infinite loop.
-    //Maybe implement a better way to generate positions?
-    //For example, make sure position is within acceptable range at generation time,
-    //so no need for checking for space.
-    //Also might want to make elipse from top and bottom similar to gif in discord.
-    //To do this, could generate y first, and then figure out acceptable bounds for x.
-    //
     //Idea for connecting events:
     //Connect paths from left to right. Progress by each Y coord of events.
     //Prevent overlapping, if overlap or no empty events to connect, connect to same event.
-    //
-    //TODO: Make sure angle from bottomPosition to first event is not the same angle for
-    //the bottomPosition to another event. Same for topPosition.
 
     private float SCREEN_HORIZONTAL_SPACING = 60f;  //Minimum spacing between sides of screen and events.
     private float SCREEN_VERTICAL_SPACING = 15f;  //Minimum spacing between top and bottom of screen and events.
@@ -30,10 +19,13 @@ public class CreateMap : MonoBehaviour
     private GameObject bottomObject;
     private GameObject topObject;
     private GameObject[][] otherObjects;
-    //private List<Vector3> otherPositions = new List<Vector3>();
+
     private int numY = 3;   //Number of Y coords to generate and spawn an event at.
-    private int minPerY = 5;    //Minimum number of events to generate for every Y coord used.
-    private int maxPerY = 5;    //Maximum number of events to generate for every Y coord used.
+    private int minPerY = 3;    //Minimum number of events to generate for every Y coord used.
+    private int maxPerY = 4;    //Maximum number of events to generate for every Y coord used.
+    private bool bottomPathsFilled = false;
+    private bool topPathsFilled = false;
+    private bool curPathsFilled = false;
 
     public GameObject enemyEncounter;
     public GameObject town;
@@ -46,6 +38,7 @@ public class CreateMap : MonoBehaviour
 
         CreateEndpoints();
         Fill();
+        EnsureAngles();
 
         for (int i = 0; i < maxPerY; i++)
         {
@@ -138,9 +131,101 @@ public class CreateMap : MonoBehaviour
         }*/
     }
 
+    //Ensures (and if necessary, corrects) angles between the bottomPositions and
+    //the first row of map locations are not too close, which would cause paths
+    //to be too close together. Does same thing for top position and last row of
+    //map locations.
+    private void EnsureAngles()
+    {
+        Vector3 curVector;
+        float curAngle;
+        Vector3 vectorToCompare;
+        float angleToCompare;
+
+        //////////////Check bottomPositions angles.
+        for (int i = 0; i < otherPositions[0].Length; i++)
+        {
+            curVector = otherPositions[0][i] - bottomPosition;
+            curAngle = Mathf.Atan2(curVector.y, curVector.x) * Mathf.Rad2Deg;
+
+            for (int j = i + 1; j < otherPositions[0].Length; j++)
+            {
+                vectorToCompare = otherPositions[0][j] - bottomPosition;
+                angleToCompare = Mathf.Atan2(vectorToCompare.y, vectorToCompare.x) * Mathf.Rad2Deg;
+
+                if (Mathf.Abs(curAngle - angleToCompare) < 10f)
+                {
+                    Vector3 newPosition = otherObjects[0][j].transform.position + new Vector3(0f, 3f, 0f);
+
+                    otherObjects[0][j].transform.position = newPosition;
+                    otherPositions[0][j] = newPosition;
+                }
+            }
+        }
+
+        curVector = otherPositions[0][otherPositions[0].Length - 1] - bottomPosition;
+        curAngle = Mathf.Atan2(curVector.y, curVector.x) * Mathf.Rad2Deg;
+        vectorToCompare = otherPositions[0][otherPositions[0].Length - 2] - bottomPosition;
+        angleToCompare = Mathf.Atan2(vectorToCompare.y, vectorToCompare.x) * Mathf.Rad2Deg;
+
+        if (Mathf.Abs(curAngle - angleToCompare) < 10f)
+        {
+            Vector3 newPosition = otherObjects[0][otherPositions[0].Length - 1].transform.position 
+                + new Vector3(0f, 3f, 0f);
+
+            otherObjects[0][otherPositions[0].Length - 1].transform.position = newPosition;
+            otherPositions[0][otherPositions[0].Length - 1] = newPosition;
+        }
+        //////////////
+
+        //////////////Check topPositions angles.
+        for (int i = 0; i < otherPositions[otherPositions.Length - 1].Length; i++)
+        {
+            curVector = otherPositions[otherPositions.Length - 1][i] - topPosition;
+            curAngle = Mathf.Atan2(curVector.y, curVector.x) * Mathf.Rad2Deg;
+
+            for (int j = i + 1; j < otherPositions[otherPositions.Length - 1].Length; j++)
+            {
+                vectorToCompare = otherPositions[otherPositions.Length - 1][j] - topPosition;
+                angleToCompare = Mathf.Atan2(vectorToCompare.y, vectorToCompare.x) * Mathf.Rad2Deg;
+
+                if (Mathf.Abs(curAngle - angleToCompare) < 10f)
+                {
+                    Vector3 newPosition = otherObjects[otherPositions.Length - 1][j].transform.position
+                        - new Vector3(0f, 3f, 0f);
+
+                    otherObjects[otherPositions.Length - 1][j].transform.position = newPosition;
+                    otherPositions[otherPositions.Length - 1][j] = newPosition;
+                }
+            }
+        }
+
+        curVector = otherPositions[otherPositions.Length - 1][otherPositions[otherPositions.Length - 1].Length - 1] 
+            - topPosition;
+        curAngle = Mathf.Atan2(curVector.y, curVector.x) * Mathf.Rad2Deg;
+        vectorToCompare = otherPositions[otherPositions.Length - 1][otherPositions[otherPositions.Length - 1].Length - 2]
+            - topPosition;
+        angleToCompare = Mathf.Atan2(vectorToCompare.y, vectorToCompare.x) * Mathf.Rad2Deg;
+
+        if (Mathf.Abs(curAngle - angleToCompare) < 10f)
+        {
+            Vector3 newPosition = 
+                otherObjects[otherPositions.Length - 1][otherPositions[otherPositions.Length - 1].Length - 1].transform.position
+                - new Vector3(0f, 3f, 0f);
+
+            otherObjects[otherPositions.Length - 1][otherPositions[otherPositions.Length - 1].Length - 1].transform.position = 
+                newPosition;
+            otherPositions[otherPositions.Length - 1][otherPositions[otherPositions.Length - 1].Length - 1] = newPosition;
+        }
+        ////////////
+    }
+
     private void GeneratePath(int curIndex)
     {
+        int startIndex = curIndex;
         float distMod = -1.75f;
+
+        curIndex = Mathf.Min(startIndex, otherPositions[0].Length - 1);
 
         GameObject curObject;
         GameObject curLine;
@@ -148,18 +233,59 @@ public class CreateMap : MonoBehaviour
         float distance = curVector.magnitude;
         float angle = Mathf.Atan2(curVector.y, curVector.x) * Mathf.Rad2Deg;
 
+        if (startIndex > otherPositions[0].Length - 1)
+        {
+            bottomPathsFilled = true;
+        }
+
         //Generates paths by spawning line in middle of two events, then scaling up.
         //Generate line from bottomObject to curObject.
-        curLine = Instantiate(line, new Vector3(
-            bottomPosition.x + (curVector.x / 2), 
-            bottomPosition.y + (curVector.y / 2), 0), Quaternion.Euler(0f, 0f, angle));
-        curLine.transform.localScale = new Vector3(distance + distMod, 1f, 1f);
+        if (!bottomPathsFilled)
+        {
+            curLine = Instantiate(line, new Vector3(
+                bottomPosition.x + (curVector.x / 2),
+                bottomPosition.y + (curVector.y / 2), 0), Quaternion.Euler(0f, 0f, angle));
+            curLine.transform.localScale = new Vector3(distance + distMod, 1f, 1f);
+        }
         curObject = otherObjects[0][curIndex];
 
         //Generate lines for filler objects.
         for (int i = 1; i < numY; i++)
         {
+            curIndex = Mathf.Min(startIndex, otherPositions[i].Length - 1);
+            if (startIndex > otherPositions[i].Length - 1 
+                && otherPositions[i].Length - 1 == otherPositions[i - 1].Length - 1)
+            {
+                curPathsFilled = true;
+            }
+            else
+            {
+                curPathsFilled = false;
+            }
             curVector = otherPositions[i][curIndex] - curObject.transform.position;
+            distance = curVector.magnitude;
+            angle = Mathf.Atan2(curVector.y, curVector.x) * Mathf.Rad2Deg;
+
+            if (!curPathsFilled)
+            {
+                curLine = Instantiate(line, new Vector3(
+                    curObject.transform.position.x + (curVector.x / 2),
+                    curObject.transform.position.y + (curVector.y / 2), 0), Quaternion.Euler(0f, 0f, angle));
+                curLine.transform.localScale = new Vector3(distance + distMod, 1f, 1f);
+            }
+            curObject = otherObjects[i][curIndex];
+        }
+
+        curIndex = Mathf.Min(startIndex, otherPositions[numY - 1].Length - 1);
+        if (startIndex > otherPositions[numY - 1].Length - 1)
+        {
+            topPathsFilled = true;
+        }
+
+        //Generate line from curObject to topObject.
+        if (!topPathsFilled)
+        {
+            curVector = topPosition - curObject.transform.position;
             distance = curVector.magnitude;
             angle = Mathf.Atan2(curVector.y, curVector.x) * Mathf.Rad2Deg;
 
@@ -167,18 +293,7 @@ public class CreateMap : MonoBehaviour
                 curObject.transform.position.x + (curVector.x / 2),
                 curObject.transform.position.y + (curVector.y / 2), 0), Quaternion.Euler(0f, 0f, angle));
             curLine.transform.localScale = new Vector3(distance + distMod, 1f, 1f);
-            curObject = otherObjects[i][curIndex];
         }
-
-        //Generate line from curObject to topObject.
-        curVector = topPosition - curObject.transform.position;
-        distance = curVector.magnitude;
-        angle = Mathf.Atan2(curVector.y, curVector.x) * Mathf.Rad2Deg;
-
-        curLine = Instantiate(line, new Vector3(
-            curObject.transform.position.x + (curVector.x / 2),
-            curObject.transform.position.y + (curVector.y / 2), 0), Quaternion.Euler(0f, 0f, angle));
-        curLine.transform.localScale = new Vector3(distance + distMod, 1f, 1f);
     }
 
     /*private void GeneratePath(int index)
@@ -211,11 +326,11 @@ public class CreateMap : MonoBehaviour
 
     private Vector3 Offset(Vector3 position)
     {
-        Vector3 angle = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
-        angle.Normalize();
+        Vector3 angleToOffset = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+        angleToOffset.Normalize();
         Vector3 convertedPosition = Camera.main.WorldToScreenPoint(position);
         Vector3 offsetPosition = Camera.main.ScreenToWorldPoint(
-            convertedPosition + (Random.Range(0f, EVENT_OFFSET) * angle));
+            convertedPosition + (Random.Range(0f, EVENT_OFFSET) * angleToOffset));
 
         return offsetPosition;
     }
