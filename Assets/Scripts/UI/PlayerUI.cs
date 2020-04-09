337294d413be65f5fd7aff2d5fbc231d22b82b09
Thana Sithisombath <sithisombath@cpp.edu>
2020-04-09 13:42:34 -0700

    test load scene additively

---
+++ Assets/Scripts/UI/PlayerUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerUI : MonoBehaviour
+{
+    public TextMeshProUGUI playerHealth;
+    public TextMeshProUGUI goldAmount;
+    public TextMeshProUGUI deckCount;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        UITest.OnHealthChange += SetPlayerHealth;
+        UITest.OnGoldChange += SetGoldAmount;
+        UITest.OnDeckChange += SetDeckCount;
+    }
+
+    public void SetPlayerHealth(int currentHealth, int maxHealth)
+    {
+        playerHealth.text = "" + currentHealth + " / " + maxHealth;
+    }
+
+    public void SetGoldAmount(int gold)
+    {
+        goldAmount.text = "" + gold;
+    }
+
+    public void SetDeckCount(int currentDeckCount)
+    {
+        deckCount.text = "" + currentDeckCount;
+    }
+}
---
+++ Assets/Scripts/UI/UITest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UITest : MonoBehaviour
+{
+    public static event Action<int, int> OnHealthChange;
+    public static event Action<int> OnGoldChange;
+    public static event Action<int> OnDeckChange;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SceneManager.LoadSceneAsync("PlayerUI", LoadSceneMode.Additive);
+    }
+
+    private void Start()
+    {
+        OnHealthChange?.Invoke(90, 100);
+        OnGoldChange?.Invoke(95);
+        OnDeckChange?.Invoke(20);
+    }
+}
